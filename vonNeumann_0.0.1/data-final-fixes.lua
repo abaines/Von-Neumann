de8686d49779f32993e675533cf6bec75679e64a
Alan Baines <abaines@vt.edu>
2019-08-20 00:27:31 -0400

    cleanup data.raw stuff

--- vonNeumann_0.0.1/data-final-fixes.lua
+++ vonNeumann_0.0.1/data-final-fixes.lua
@@ -1,17 +1,12 @@
 -- Kizrak
 
---data.raw.character.inventory_size = 1
-data.raw["character"]["character"].inventory_size = 2
 data.raw.character.character.inventory_size = 1
-local gc = data.raw["god-controller"]
+local godController = data.raw["god-controller"]
 
-print(serpent.block(gc))
-log(serpent.block(gc))
+print(serpent.block(godController))
+log(serpent.block(godController))
 
-gc.default.inventory_size = 1
-
---local gc2 = gc["god-controller"]
---gc2.inventory_size = 1
+godController.default.inventory_size = 1
 
 print("data-final-fixes.lua")
 log("data-final-fixes.lua")
