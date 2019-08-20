99a6c16b52428efc03c1c0c167d77cd025cb64f4
Alan Baines <abaines@vt.edu>
2019-08-20 00:52:24 -0400

    destroy the flesh!

--- vonNeumann_0.0.1/vonNeumann.lua
+++ vonNeumann_0.0.1/vonNeumann.lua
@@ -2,23 +2,42 @@
 
 --- needs "hijack"
 
-
-function newPlayer(event)
-	game.print("newPlayer: "..serpent.block(event.player_index),{r=255,g=255})
-
-	for i, player in pairs(game.players) do
-		local playerCharacter = player.character
-		player.character = nil
-		--playerCharacter.destroy()
-	end
-
-	local numberPlayers = #game.players
-	local msg = "newPlayer complete: " .. numberPlayers
+function kprint(msg)
 	print(msg)
 	log(msg)
 	if not game.is_multiplayer() then
 		game.print(msg,{r=255,g=255})
 	end
+end
+
+
+function newPlayer(event)
+	kprint("newPlayer: "..serpent.block(event.player_index),{r=255,g=255})
+
+	for i, player in pairs(game.players) do
+		local playerCharacter = player.character
+		player.character = nil
+	end
+
+	for i, entity in pairs(game.surfaces[1].find_entities_filtered{}) do -- iterate through entities
+		if entity.type == "character" then -- it's an inserter, do something to it
+			--kprint("T::"..entity.type)
+			--kprint("E::"..serpent.block(entity))
+			entity.destroy()
+		end
+	end
+
+	--[[playerCharacter.destroy()
+	local characters = game.surfaces[1].find_entities_filtered{type= "character"}
+	kprint("CS::" .. serpent.block(characters))
+	for character in pairs(characters) do
+		kprint("C::" .. serpent.block(character))
+		character.destory()
+	end]]--
+
+	local numberPlayers = #game.players
+	local msg = "newPlayer complete: " .. numberPlayers
+	kprint(msg)
 end
 
 
