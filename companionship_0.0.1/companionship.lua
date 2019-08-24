50714497c16f762719749f231e8a5e61da5ed4a6
Alan Baines <abaines@vt.edu>
2019-08-24 15:28:27 -0400

    ignore on_player_created msgs

--- companionship_0.0.1/companionship.lua
+++ companionship_0.0.1/companionship.lua
@@ -10,9 +10,10 @@ function companionship(event)
 	local min_companions = settings.startup['minimum_number_of_companions'].value
 	local slow_speed_companion = settings.startup['speed_when_below_minimum_number_of_companions'].value
 
+	local eventName = eventNameMapping[event.name]
 	local numberConnectedPlayers = #game.connected_players
 
-	local msg = "Companionship: "..numberConnectedPlayers .. " / " .. min_companions .. "  =  " .. game.speed .. "    (" .. eventNameMapping[event.name] ..")"
+	local msg = "Companionship: "..numberConnectedPlayers .. " / " .. min_companions .. "  =  " .. game.speed .. "    [" .. eventName .."]"
 	log(msg)
 
 	if numberConnectedPlayers < min_companions then
@@ -21,7 +22,9 @@ function companionship(event)
 		game.speed = 1
 	end
 
-	game.print(msg,{r=255,g=255})
+	if eventName ~= "on_player_created" then
+		game.print(msg,{r=255,g=255})
+	end
 end
 
 
