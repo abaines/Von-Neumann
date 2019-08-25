c6226af9361f3fbb995f8f349c030eeaef365c5b
Alan Baines <abaines@vt.edu>
2019-08-25 11:52:45 -0400

    campaign!?!

---
+++ vonNeumann_0.0.5/campaigns/Von Neumann/level-01/control.lua
@@ -0,0 +1,11 @@
+-- Kizrak
+
+local handler = require("event_handler")
+handler.add_lib(require("silo-script"))
+
+require("hijack")
+require("playerBonuses")
+require("vonNeumann")
+
+print("control.lua")
+log("control.lua")
