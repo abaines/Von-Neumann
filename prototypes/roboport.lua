a2f859fe095bf5e13ae8c0e3fdc31468fe6ec233
Alan Baines <abaines@vt.edu>
2019-12-13 21:47:03 -0500

    Update roboport.lua
    so many charging ports!

--- prototypes/roboport.lua
+++ prototypes/roboport.lua
@@ -18,8 +18,10 @@ end
 local roboport = table.deepcopy( data.raw["roboport"]["roboport"] )
 roboport.name = "vn-roboport"
 roboport.order = "c[signal]-a[roboport]"
+
 roboport.construction_radius = 55*3
 roboport.dying_explosion = "massive-explosion"
+
 roboport.energy_usage = "0kW"
 roboport.charging_energy = "100000kW"
 roboport.recharge_minimum = "400MJ"
@@ -29,8 +31,22 @@ roboport.energy_source = {
 	type = "electric",
 	usage_priority = "secondary-input"
 }
+
 roboport.material_slots_count = 20
 roboport.robot_slots_count = 20
+
+
+local number_ports = 4*20
+local port_radius = 1.5
+roboport.charging_offsets = {}
+
+for i=360/number_ports,360,360/number_ports do
+	local x = port_radius * math.sin( math.rad(i) )
+	local y = port_radius * math.cos( math.rad(i) )
+	table.insert(roboport.charging_offsets,{x,y})
+end
+
+roboport.charge_approach_distance = 2
 
 
 data:extend{roboport}
