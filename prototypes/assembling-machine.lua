8e63682cfc5a3f4eb11f0bc9e8bd177402fe63ed
Alan Baines <abaines@vt.edu>
2019-12-30 17:01:43 -0500

    graphics

--- prototypes/assembling-machine.lua
+++ prototypes/assembling-machine.lua
@@ -3,6 +3,11 @@
 
 local sb = serpent.block
 
+-- replace __base__ path with __vonNeumann__
+local function pathReplace(base)
+	return string.gsub(base,"__base__","__vonNeumann__")
+end
+
 
 local item = table.deepcopy( data.raw.item["assembling-machine-1"] )
 item.name = "damaged-assembling-machine"
@@ -17,6 +22,11 @@ assembling_machine.order = "a[assembling-machine-1]"
 assembling_machine.subgroup = "production-machine"
 assembling_machine.minable.result = "damaged-assembling-machine"
 assembling_machine.next_upgrade = "assembling-machine-1"
+
+-- graphics
+local layer1 = assembling_machine.animation.layers[1]
+layer1.filename = pathReplace(layer1.filename)
+layer1.hr_version.filename = pathReplace(layer1.hr_version.filename)
 
 
 data:extend{assembling_machine}
