f72ccbd14b1fc5ddc479ebbb7dcdd43e361e9667
Alan Baines <abaines@vt.edu>
2020-01-28 23:42:18 -0500

    "factorio_version": "0.18"

--- prototypes/belt.lua
+++ prototypes/belt.lua
@@ -15,6 +15,7 @@ local grey_belt_item = table.deepcopy(data.raw.item["transport-belt"])
 grey_belt_item.name = "vn-transport-belt"
 grey_belt_item.place_result = "vn-transport-belt"
 grey_belt_item.icon = pathReplace(grey_belt_item.icon)
+grey_belt_item.icon_size = 32
 grey_belt_item.order = "a[transport-belt]-Z[transport-belt]"
 
 --log( "grey_belt_item" )
@@ -34,6 +35,7 @@ grey_belt_entity.circuit_wire_connection_points = nil
 grey_belt_entity.circuit_wire_max_distance = nil
 
 grey_belt_entity.icon = pathReplace(grey_belt_entity.icon)
+grey_belt_entity.icon_size = 32
 local animation_set = grey_belt_entity.belt_animation_set.animation_set
 animation_set.filename = pathReplace(animation_set.filename)
 animation_set.hr_version.filename = pathReplace(animation_set.hr_version.filename)
