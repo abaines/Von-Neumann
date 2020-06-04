0cb995bd1f04ef7d76737d3bc4ea703d34611b2f
Alan Baines <abaines@vt.edu>
2020-06-04 15:44:10 -0400

    Update permissions.lua

--- scenarios/Von Neumann/permissions.lua
+++ scenarios/Von Neumann/permissions.lua
@@ -14,24 +14,10 @@ end
 
 local permissions = {}
 
-
 -- /toggle-action-logging
-
 -- https://lua-api.factorio.com/latest/defines.html#defines.input_action
 -- https://lua-api.factorio.com/latest/LuaPermissionGroup.html#LuaPermissionGroup.set_allows_action
 -- https://factorioconsolecommands.com/factorio-console-commands/toggle-action-logging-g2893r8
-
-
-log("new code goes here")
-
-
---  748.025 Info GameActionHandler.cpp:301: Action performed [108075 0 ChangePickingState]
---  863.674 Info GameActionHandler.cpp:301: Action performed [115014 0 ResetAssemblingMachine]
---  914.141 Info GameActionHandler.cpp:301: Action performed [118042 0 CursorTransfer]
---  932.592 Info GameActionHandler.cpp:301: Action performed [119149 0 InventoryTransfer]
--- 1107.146 Info GameActionHandler.cpp:301: Action performed [129622 0 BeginMining]
--- 1136.481 Info GameActionHandler.cpp:301: Action performed [131382 0 FastEntityTransfer]
--- 1523.457 Info GameActionHandler.cpp:301: Action performed [154588 0 PasteEntitySettings]
 
 
 local input_actionNameMapping = {}
@@ -95,8 +81,6 @@ end
 script.on_event({
 	defines.events.on_player_joined_game,
 },on_player_joined_game)
-
-
 
 
 script.register_object(permissions)
