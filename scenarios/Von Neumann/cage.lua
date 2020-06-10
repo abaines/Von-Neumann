82d9df2a9c895c6b861ba239a96a593bcab831de
Alan Baines <abaines@vt.edu>
2020-06-10 01:31:50 -0400

    Update cage.lua
    validPosition

--- scenarios/Von Neumann/cage.lua
+++ scenarios/Von Neumann/cage.lua
@@ -9,6 +9,26 @@ local sb = serpent.block -- luacheck: ignore 211
 local cage = {}
 
 
+local function validPosition(player)
+	local x = math.floor(player.position.x / 32)
+	local y = math.floor(player.position.y / 32)
+
+	local force = player.force
+	local surface = player.surface
+
+	for xx=-2,2 do
+		for yy=-2,2 do
+			local pos = {x+xx,y+yy}
+			if not force.is_chunk_charted(surface,pos) then
+				return false
+			end
+		end
+	end
+
+	return true
+end
+
+
 local function on_player_changed_position(event)
 	local player = game.players[event.player_index]
 	log(sb( event ))
