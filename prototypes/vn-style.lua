84d3d2c1fce5ffe9111a1e4c0b499130680c10c1
Alan Baines <abaines@vt.edu>
2020-06-22 21:22:03 -0400

    Update vn-style.lua
    opacity for draggable_space

--- prototypes/vn-style.lua
+++ prototypes/vn-style.lua
@@ -16,8 +16,24 @@ local vn_frame = table.deepcopy(gui_style_default.frame)
 
 vn_frame.graphical_set.base.opacity = 0.15
 vn_frame.graphical_set.base.background_blur = false
+vn_frame.header_filler_style.parent = "vn-draggable_space_header"
 
 gui_style_default['vn-frame'] = vn_frame
+
+
+local vn_header = table.deepcopy(gui_style_default["draggable_space_header"] )
+
+vn_header.parent = "vn-draggable_space"
+
+gui_style_default['vn-draggable_space_header'] = vn_header
+
+
+local vn_space = table.deepcopy(gui_style_default["draggable_space"] )
+
+vn_space.graphical_set.base.opacity = 0.15
+
+gui_style_default['vn-draggable_space'] = vn_space
+
 
 log(string.rep("+",80))
 
@@ -51,5 +67,8 @@ log(sb( key_array ))
 
 log(string.rep("=",80))
 
-log(sb( vn_frame ))
+
+log(sb( gui_style_default["vn-frame"] ))
+log(sb( gui_style_default["vn-draggable_space_header"] ))
+log(sb( gui_style_default["vn-draggable_space"] ))
 
