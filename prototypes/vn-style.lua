a14eaa04f993ebe2f2812a6aec3dc3caf2052f95
Alan Baines <abaines@vt.edu>
2020-06-22 20:22:55 -0400

    Update vn-style.lua

--- prototypes/vn-style.lua
+++ prototypes/vn-style.lua
@@ -9,18 +9,30 @@ local function isTable(t)
 end
 
 
-local gui_style = table.deepcopy( data.raw['gui-style'] )
+local gui_style_default = data.raw['gui-style'].default
+
+
+local default_frame = table.deepcopy(gui_style_default.frame)
+
+default_frame.graphical_set.opacity = 0.15
+
+log(sb( default_frame ))
+
+gui_style_default['vn-frame'] = default_frame
 
 log(string.rep("+",80))
 
 local extras = {}
 
-for k,v in pairs(gui_style.default) do
+for k,v in pairs(gui_style_default) do
 	if isTable(v) then
-		log(k .. '   [' .. v.type .. ']')
-		for k2,v2 in pairs(v) do
-			if not (k2=="type") then
-				log("   " .. k2)
+		local vtype = v.type
+		if string.find(vtype,"frame") then
+			log(k .. '   [' .. vtype .. ']')
+			for k2,v2 in pairs(v) do
+				if not (k2=='type') then
+					log("   " .. k2)
+				end
 			end
 		end
 	else
