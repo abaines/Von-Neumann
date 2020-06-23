5d773a81eef37728e4885a035a7d8ed87fb859f9
Alan Baines <abaines@vt.edu>
2020-06-22 20:59:15 -0400

    Update vn-style.lua
    improve logging

--- prototypes/vn-style.lua
+++ prototypes/vn-style.lua
@@ -12,17 +12,17 @@ end
 local gui_style_default = data.raw['gui-style'].default
 
 
-local vn_frame = table.deepcopy(gui_style_default.blurry_frame)
+local vn_frame = table.deepcopy(gui_style_default.frame)
 
 vn_frame.graphical_set.base.opacity = 0.15
-
-log(sb( vn_frame ))
+vn_frame.graphical_set.base.background_blur = false
 
 gui_style_default['vn-frame'] = vn_frame
 
 log(string.rep("+",80))
 
 local extras = {}
+local keys = {}
 
 for k,v in pairs(gui_style_default) do
 	if isTable(v) then
@@ -30,9 +30,10 @@ for k,v in pairs(gui_style_default) do
 		if string.find(vtype,"frame") then
 			log(k .. '   [' .. vtype .. ']')
 			for k2,v2 in pairs(v) do
-				if not (k2=='type') then
+				if not (k2=='type') and false then
 					log("   " .. k2)
 				end
+				keys[k2] = true
 			end
 		end
 	else
@@ -40,7 +41,15 @@ for k,v in pairs(gui_style_default) do
 	end
 end
 
+log(sb( extras ))
+
+local key_array = {}
+for k,_ in pairs(keys) do
+	table.insert(key_array,k)
+end
+log(sb( key_array ))
+
 log(string.rep("=",80))
 
-log(sb( extras ))
+log(sb( vn_frame ))
 
